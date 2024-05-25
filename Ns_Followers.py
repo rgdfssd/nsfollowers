349695a6b298b89cc4347dbdb35988389b1a3a65
rgdfssd <161238073+rgdfssd@users.noreply.github.com>
2024-05-26 02:41:09 +0530

    Update Ns_Followers.py

--- Ns_Followers.py
+++ Ns_Followers.py
@@ -11,9 +11,9 @@ class Ns_Followers:
         self.requests=requests
         self.__AES_NoPadding = AES_CBC_NoPadding()
 
-        self.__userid='...' #Enter UserID Here
-        self.__username='...' #Enter UserName Here
-        self._session='...' #Enter SessionID Here
+        self.__userid='63918693174' #Enter UserID Here
+        self.__username='powerispower506' #Enter UserName Here
+        self._session='63918693174%3AGYdDvbNkBmQrVT%3A1%3AAYfNSp2C0vpHXpPocQbgqB9rJPVtnOAQ6Ub4I5gBZA' #Enter SessionID Here
 
         self.__Pk=self.__userid.encode()+b'*'+str(random.randint(111,999)).encode()
         self.__Ip=os.urandom(8).hex().encode()+b'*'+str(random.randint(111,999)).encode()
